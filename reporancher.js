f0f795f109f7749ac34dca13a4be91fa487863a1
John Harrison <john@cloudant.com>
2013-11-21 20:03:54 -0800

    Add turn based battle sim

--- reporancher.js
+++ reporancher.js
@@ -1,3 +1,4 @@
+
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
     return "Welcome to reporancher.";
@@ -5,9 +6,19 @@ if (Meteor.isClient) {
 
   Template.hello.events({
     'click input' : function () {
+      // get data from form template
+      var form_data = {
+        "user1": "bob",
+        "repo1": "reponame1", 
+        "user2": "alice",
+        "repo2" : "reponame2"
+      };
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+        console.log("Here is the data: " + data);
+      // make API calls here using data
+      // ...
+      // render the new template with both monsters (passed in?)
     }
   });
 }
@@ -17,3 +28,49 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+function Monster(data) {
+  // constructor for monsters
+  this.user =  data; //data.user;
+  this.name = data; //data.repo;
+  this.maxHealth = 100;
+  this.currentHealth = this.maxHealth;
+  this.alive = true;
+  this.strength = 100;
+  this.accuracy = 1;
+  this.defense = 0.00;
+};
+
+function fight() {
+  var monster1 = new Monster("foo");
+  var monster2 = new Monster("bar");
+  console.log("Let the battle begin!");
+  var rounds = 0; // round counter ( interesting for stats maybe )
+  while(monster1.alive && monster2.alive) {
+    console.log(attack(monster1, monster2));
+    console.log(attack(monster2, monster1));
+    console.log(monster1.currentHealth + ": " + monster2.currentHealth);
+    // write result of this round to input box
+  }
+  if (!monster1.alive && !monster2.alive) {
+    console.log("Both monsters died! It is a draw!!");
+  } else {
+    var winner = (monster1.alive) ? monster1.name : monster2.name;
+    console.log(winner + " is victorious!");
+  }
+};
+
+function attack(attacker, defender) {
+  // simulates a single attack, returns "miss" if missed
+  // else returns damage as int (factoring in defense)
+  if (Math.floor((Math.random()*100)+1) > attacker.accuracy) {
+    return attacker.name + " misses";
+  } else {
+    // hits
+    defender.currentHealth -= Math.round(attacker.strength * (1-defender.defense));
+    if (defender.currentHealth <= 0)
+      defender.alive = false;
+    return attacker.name + " hits " + defender.name + " for " + Math.round(attacker.strength * (1-defender.defense));
+  }
+};
+
